9b2604101f1a7e860dec5c9996971d098487f326
Tim Watt <timothy.watt@cloudsherpas.com>
2015-06-08 19:50:02 -0400

    Another attempt at fixing build path issue

--- sforganalyzer.py
+++ sforganalyzer.py
@@ -4,9 +4,9 @@ from xml.dom.minidom import parseString
 def runAnt(self,target):
 	directory = os.path.join(sublime.packages_path(), "SalesforceOrgAnalyzer", "salesforce-org-analyzer")
 	self.build_file =os.path.join(directory, "build.xml")
-	
+
 	if not os.path.exists(directory):
-    	os.makedirs(directory)
+		os.makedirs(directory)
 
 	if os.path.isfile(self.build_file) == False:
 		fh = open(os.path.dirname(os.path.realpath(__file__)), 'rb')
